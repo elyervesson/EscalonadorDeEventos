134b1d78529736c64c0d924a4660068174e9ba68
Bruno Rocha <bruno.rocha@ccc.ufcg.edu.br>
2017-07-14 23:22:42 -0300

    Update Fregues.java

--- src/core/Fregues.java
+++ src/core/Fregues.java
@@ -12,8 +12,14 @@ public class Fregues{
 	public Fregues() {
 
 		gerador = new Random();
-		tempoDeChegada = gerador.nextInt(10) + 1; // de 1 a 10 segundos
 		tipoFila = gerador.nextInt(2) + 1; // do tipo 1 ou 2
+		
+		if (tipoFila == 1) {
+			tempoDeChegada = gerador.nextInt(10) + 1; // de 1 a 10 segundos
+		} else if (tipoFila == 2) {
+			tempoDeChegada = gerador.nextInt(5) + 1; // de 1 a 5 segundos
+		}
+		
 		idFregues = gerador.nextInt(500);
 	}
 
